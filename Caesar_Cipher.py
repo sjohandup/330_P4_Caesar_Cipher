6f0901b359e7ab71645bc24abbec14f3e89e6dc1
Mazzl3s <mperoski@outlook.com>
2017-10-06 19:25:54 +0200

    Added skeleton

---
+++ Caesar_Cipher.py
@@ -0,0 +1,11 @@
+# Variable to store text from file
+global file_Sting = ""
+
+# Function that reads from a file
+def function read_File ()
+
+# Function that deciphers the cipher
+def function ceaser_Cipher ()
+
+# Function that prints from a file
+def function print_File ()

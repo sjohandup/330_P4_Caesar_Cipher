52d06682d4751fcd2f0486a39d23da03be1c5369
Mazzl3s <mperoski@outlook.com>
2017-10-06 19:47:24 +0200

    Added code to cipher function

--- Caesar_Cipher.py
+++ Caesar_Cipher.py
@@ -7,9 +7,18 @@ def read_File () :
 
 # Function that deciphers the cipher
 def ceaser_Cipher () :
+    # Call variable that stores cipher
+    global file_Sting
+    # Variables that store point breaks in ascii table
     numbers = 48
     small_Caps = 65
     large_Caps = 97
+    # Counter variable
+    count = 61
+
+    for i in range (0, count) :
+        for c in range (0, len (file_Sting)) :
+            print ("DUMMY")
 
 # Function that prints from a file
 def print_File () :

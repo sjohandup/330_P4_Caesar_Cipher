bd601397446f3b9ccbefbd5cb98c050d547b3b67
Mazzl3s <mperoski@outlook.com>
2017-10-06 20:45:44 +0200

    Denk code 3

--- Caesar_Cipher.py
+++ Caesar_Cipher.py
@@ -23,40 +23,40 @@ def write_File (decodedArray) :
 # Function that deciphers the cipher
 def ceaser_Cipher () :
     # Call variable that stores cipher
-    global file_Sting
+	global file_Sting
     # Variables that store point breaks in ascii table
-    numbers = 57
-    large_Caps = 90
-    small_Caps = 122
+	numbers = 57
+	large_Caps = 90
+	small_Caps = 122
     # Counter variable
-    count = 61
+	count = 61
     # String array to store all of the iterations
-    iterations = []
-    # Append cipher
-    iteration.append (file_Sting)
+	iterations = []
+	# Append cipher
+	iterations.append (file_String)
 
-    # Loop through the counter
-    for i in range (1, count) :
-        # Loop through each of the characters
-        for c in range (0, len (file_Sting)) :
-            # Check if the max number ascii value has been reached
-            if (ord (file_Sting[c]) == number) :
-                file_Sting[c] = chr (ord (file_Sting[c]) + 8)
-            # Check if the max large caps ascii value has been reached
-            elif (ord (file_Sting[c]) == large_Caps) :
-                file_Sting[c] = chr (ord (file_Sting[c]) + 7)
-            # Check if the max small caps ascii value has been reached
-            elif (ord (file_Sting[c]) == small_Caps) :
-                file_Sting[c] = chr (ord (file_Sting[c]) - 74)
-            else :
-                file_Sting[c] = chr (ord (file_Sting[c]) + 1)
-        # Print the iteration count
-        print ("Iteration: " + count)
-        # Print the cipher
-        print (file_Sting + "\n")
-        # Append to the string file
-        iteration.append (file_Sting)
-    # Call write file function
+	# Loop through the counter
+	for i in range (1, count) :
+		# Loop through each of the characters
+		for c in range (0, len (file_String)) :
+			# Check if the max number ascii value has been reached
+			if (ord (file_String[c]) == numbers) :
+				file_String[c] = chr (ord (file_String[c]) + 8)
+			# Check if the max large caps ascii value has been reached
+			elif (ord (file_String[c]) == large_Caps) :
+				file_String[c] = chr (ord (file_String[c]) + 7)
+			# Check if the max small caps ascii value has been reached
+			elif (ord (file_String[c]) == small_Caps) :
+				file_String[c] = chr (ord (file_String[c]) - 74)
+			else :
+				file_String[c] = chr (ord (file_String[c]) + 1)
+		# Print the iteration count
+		print ("Iteration: " + count)
+		# Print the cipher
+		print (file_String + "\n")
+		# Append to the string file
+		iterations.append (file_String)
+	# Call write file function
 	write_File (iteration)
 
 # Function that prints from a file
@@ -66,7 +66,6 @@ def print_File () :
 	decoded_file_String = encoded_txt.read()
 	encoded_txt.close()
 	print (decoded_file_String)
-
 
 # Main function
 def main () :

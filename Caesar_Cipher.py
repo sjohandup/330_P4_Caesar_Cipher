a7fc84742c8e653dbf3f2bc4c5958845c3ad11e0
Mazzl3s <mperoski@outlook.com>
2017-10-06 19:40:43 +0200

    Added more code

--- Caesar_Cipher.py
+++ Caesar_Cipher.py
@@ -2,10 +2,23 @@
 global file_Sting = ""
 
 # Function that reads from a file
-def function read_File ()
+def function read_File () :
+
 
 # Function that deciphers the cipher
-def function ceaser_Cipher ()
+def function ceaser_Cipher () :
+    numbers = 48
+    small_Caps = 65
+    large_Caps = 97
 
 # Function that prints from a file
-def function print_File ()
+def function print_File () :
+
+
+# Main function
+def main () :
+
+
+# Run main module
+if (__name__ == "__main__") :
+    main()

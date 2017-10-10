b1a020f2fbe095300cacc0344d1aa1e9059359a1
Mazzl3s <mperoski@outlook.com>
2017-10-10 22:01:07 +0200

    FINAL

--- findMessage.py
+++ findMessage.py
@@ -1,8 +1,15 @@
 # SJ du Plooy	- 12070794
 # M  Peroski	- 13242475
 
+# Import libraries
+import os
+
 # Main function
 def main () :
+	# Remove the denk
+	if (os.path.isfile('./human_Readable.txt')) :
+		os.remove("human_Readable.txt")
+
 	# Variable to store entire text file as string
 	decoded_str = ""
 	end_result_str = ""
@@ -11,46 +18,58 @@ def main () :
 	next_word_index = 0
 
 	# Open and retrieve decoded file text, and close file when done
-	decoded_file = open("temp_decoded.txt", "r")
-	decoded_str = decoded_file.read()
+	decoded_file = open("decoded.txt", "r")
+	decoded_str = decoded_file.read().splitlines()
 	decoded_file.close()
 
-	while (next_word_index < len(decoded_str)) :
-		print ("next_word_index =", next_word_index)
-		print (decoded_str[next_word_index]+".txt")
+	output = open("human_Readable.txt", "a")
 
-		dictionary_str = ""
-		dictionary_arr = []
+	for x in range (0, len(decoded_str)) :
+		decoded_Line = decoded_str[x]
+		next_word_index = 0
+		end_result_str = ""
+		while (next_word_index < len(decoded_Line)) :
+			if (ord(decoded_Line[next_word_index]) >= 97 and ord(decoded_Line[next_word_index]) <= 122) :
 
-		if ((decoded_str[next_word_index]) == "\n") :
-			break
+				dictionary_str = ""
+				dictionary_arr = []
 
-		dictionary_file = open("Seperated/"+decoded_str[next_word_index]+".txt", "r+")
-		dictionary_str = dictionary_file.read()
-		dictionary_file.close()
-		dictionary_arr = dictionary_str.split("\n")
+				if ((decoded_Line[next_word_index]) == "\n") :
+					break
 
-		print ("length of dictionary_arr", len(dictionary_arr))
+				dictionary_file = open("Seperated/"+decoded_Line[next_word_index]+".txt", "r+")
+				dictionary_str = dictionary_file.read()
+				dictionary_file.close()
+				dictionary_arr = dictionary_str.split("\n")
 
-		temp = decoded_str[next_word_index : len(decoded_str)]
+				temp = decoded_Line[next_word_index : len(decoded_Line)]
 
-		match_index = 0
-		match_found = 0
-		j = 0
-		while (j < len(dictionary_arr) and match_found == 0) :
-			if ((len(temp) - next_word_index) < len(dictionary_arr[j])) :
+				match_index = 0
 				match_found = 0
-			if (dictionary_arr[j] == temp[0 : len(dictionary_arr[j])]) :
-				print (dictionary_arr[j])
-				print (decoded_str[next_word_index : ])
-				end_result_str = end_result_str + dictionary_arr[j] + " "
-				match_index = j
-				match_found = -1
+				j = 0
+				while (j < len(dictionary_arr) and match_found == 0) :
+					if ((len(temp) - next_word_index) < len(dictionary_arr[j])) :
+						match_found = 0
+					if (dictionary_arr[j] == temp[0 : len(dictionary_arr[j])]) :
+						end_result_str = end_result_str + dictionary_arr[j] + " "
+						match_index = j
+						match_found = -1
+					else :
+						j = j+1
+				next_word_index = next_word_index + len(dictionary_arr[match_index])
 			else :
-				j = j+1
-		next_word_index = next_word_index + len(dictionary_arr[match_index])
+				end_result_str = end_result_str + decoded_Line[next_word_index]
 
-	print (end_result_str)
+				if (next_word_index + 1 < len (decoded_Line)) :
+					if (ord(decoded_Line[next_word_index + 1]) >= 97 and ord(decoded_Line[next_word_index + 1]) <= 122) :
+						end_result_str = end_result_str + " "
+
+				next_word_index = next_word_index + 1
+
+		print (end_result_str)
+		output.write(end_result_str)
+		output.write("\n")
+	output.close()
 
 # Run main module
 if (__name__ == "__main__") :
